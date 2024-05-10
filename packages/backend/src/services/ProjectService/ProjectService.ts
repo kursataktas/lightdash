ab349b4d9fca9f061fd866728e4653deed843f55
Jose Rego <jose@lightdash.com>
2024-05-10 09:51:01 +0100

    fix: check permission for total calculation and csv download

--- packages/backend/src/services/ProjectService/ProjectService.ts
+++ packages/backend/src/services/ProjectService/ProjectService.ts
@@ -3862,6 +3862,17 @@ export class ProjectService extends BaseService {
         ) {
             throw new ForbiddenError();
         }
+        if (
+            data.metricQuery.customDimensions?.some(isCustomSqlDimension) &&
+            user.ability.cannot(
+                'manage',
+                subject('CustomSql', { organizationUuid, projectUuid }),
+            )
+        ) {
+            throw new ForbiddenError(
+                'User cannot run queries with custom SQL dimensions',
+            );
+        }
 
         const results = await this._calculateTotal(
             user,
